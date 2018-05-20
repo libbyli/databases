8e8d60e30e774e17bff365e45e089766a7bed4c2
libbyli <sincerelylibby@gmail.com>
2018-05-19 17:28:53 -0700

    Mostly implemented get and post requests for messages.

--- server/models/index.js
+++ server/models/index.js
@@ -11,38 +11,49 @@ module.exports = {
     }, // a function which produces all the messages
     post: function (request) {
       let roomID;
+      let userID;
 
       db.connection.query(`INSERT INTO rooms (name) values ('${request.body.roomname}')`, (err, result) => {
         if (err) { throw err; }
       });
 
-      db.connection.query(`SELECT roomID from rooms where name='${request.body.roomname}'`, (err, rows) => {
-        if (err) { throw err; }
-        roomID = JSON.parse(JSON.stringify(rows));
-        roomID = roomID[0].roomID;
-      });
-
-      console.log('roomID ------> ', roomID);
-
-      let userID;
-
       db.connection.query(`INSERT INTO users (name) values ('${request.body.username}')`, (err, result) => {
         if (err) { throw err; }
       });
 
-      db.connection.query(`SELECT userID from users where name='${request.body.username}'`, (err, rows) => {
-        if (err) { throw err; }
-        userID = JSON.parse(JSON.stringify(rows));
-        userID = userID[0].userID;
-      });
+      const promiseRoom = () => {
+        return new Promise((resolve, reject) => {
+          db.connection.query(`SELECT roomID from rooms where name='${request.body.roomname}'`, (err, rows) => {
+            if (err) { 
+              reject(err); 
+            } else {
+              roomID = JSON.parse(JSON.stringify(rows));
+              roomID = roomID[0].roomID;
+              resolve(roomID);
+            }
+          });
+        });
+      };
 
-      console.log('userID ------> ', userID);
+      const promiseUser = () => {
+        return new Promise((resolve, reject) => {
+          db.connection.query(`SELECT userID from users where name='${request.body.username}'`, (err, rows) => {
+            if (err) { 
+              reject(err); 
+            } else {
+              userID = JSON.parse(JSON.stringify(rows));
+              userID = userID[0].userID;
+              resolve(userID);
+            }
+          });
+        });
+      };
 
-      // Promise.all([roomID, userID]).then(function() {
-      //   db.connection.query(`INSERT INTO messages (message, userID, roomID) VALUES ('${request.body.message}', ${userID}, ${roomID})`, (err, result) => {
-      //     if (err) { throw err; }
-      //   })
-      // });  
+      Promise.all([promiseRoom(), promiseUser()]).then((array) => {
+        db.connection.query(`INSERT INTO messages (message, userID, roomID) VALUES ('${request.body.text}', ${userID}, ${roomID})`, (err, result) => {
+          if (err) { throw err; }
+        });
+      });  
     } 
     // a function which can be used to insert a message into the database
   },
